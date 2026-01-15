260277f2641de7ebd2db91691ddc6ee6cb5b2022
lorey2 <lorey@student.42lausanne.ch>
2026-01-15 17:29:58 +0100

    Refactor treesitter configuration for clarity

--- lua/plugins/builtin/treesitter.lua
+++ lua/plugins/builtin/treesitter.lua
@@ -1,43 +1,20 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  version = false, 
-  build = ":TSUpdate",
-  branch = "master", -- IMPORTANT: Forces the stable branch to fix your crash
-  event = { "BufReadPost", "BufNewFile" },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      -- Add languages you need here
-      ensure_installed = { "c", "cpp", "go", "lua", "vim", "vimdoc", "python", "bash" },
-      
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    branch = "master", -- FIXED: Forces the stable branch to prevent the crash
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            -- Add languages you need here
+            ensure_installed = { "c", "cpp", "go", "lua", "python", "bash", "vim", "vimdoc" },
 
-      -- Automatically install missing parsers when entering buffer
-      auto_install = true,
-
-      highlight = {
-        enable = true, -- false will disable the whole extension
-        additional_vim_regex_highlighting = false,
-      },
-      
-      indent = { enable = true },
-      
-      -- Config for nvim-treesitter-textobjects
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-          },
-        },
-      },
-    })
-  end,
+            auto_install = true,
+            sync_install = false,
+            
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end,
 }
