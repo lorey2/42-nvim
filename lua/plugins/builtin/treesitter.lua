d741ca506fe9fde4ea29bbfb7858f200f09e0724
lorey2 <lorey@student.42lausanne.ch>
2026-01-15 17:03:20 +0100

    Update treesitter.lua

--- lua/plugins/builtin/treesitter.lua
+++ lua/plugins/builtin/treesitter.lua
@@ -1,8 +1,20 @@
 return {
-	-- Highlight, edit, and navigate code
-	'nvim-treesitter/nvim-treesitter',
-	dependencies = {
-		'nvim-treesitter/nvim-treesitter-textobjects',
-	},
-	build = ':TSUpdate',
+  {
+    -- Main Treesitter Engine
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      -- This is where your main treesitter setup goes
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        -- Add any other treesitter settings you had here
+      })
+    end,
+  },
+  {
+    -- Treesitter Extensions (Must load AFTER the main engine)
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+  },
 }
