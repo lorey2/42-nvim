0f06edfedc9c137d2850f1745ba44652b907d739
lorey2 <lorey@student.42lausanne.ch>
2026-01-15 17:26:56 +0100

    Update Treesitter configuration and dependencies

--- lua/plugins/builtin/treesitter.lua
+++ lua/plugins/builtin/treesitter.lua
@@ -1,20 +1,43 @@
 return {
-  {
-    -- Main Treesitter Engine
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      -- This is where your main treesitter setup goes
-      require("nvim-treesitter.configs").setup({
-        highlight = { enable = true },
-        indent = { enable = true },
-        -- Add any other treesitter settings you had here
-      })
-    end,
-  },
-  {
-    -- Treesitter Extensions (Must load AFTER the main engine)
+  "nvim-treesitter/nvim-treesitter",
+  version = false, 
+  build = ":TSUpdate",
+  branch = "master", -- IMPORTANT: Forces the stable branch to fix your crash
+  event = { "BufReadPost", "BufNewFile" },
+  dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
   },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      -- Add languages you need here
+      ensure_installed = { "c", "cpp", "go", "lua", "vim", "vimdoc", "python", "bash" },
+      
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+
+      -- Automatically install missing parsers when entering buffer
+      auto_install = true,
+
+      highlight = {
+        enable = true, -- false will disable the whole extension
+        additional_vim_regex_highlighting = false,
+      },
+      
+      indent = { enable = true },
+      
+      -- Config for nvim-treesitter-textobjects
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
+        },
+      },
+    })
+  end,
 }
