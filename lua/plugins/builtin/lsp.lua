e715ed7a131c05a104bce8dc201d0fb0c615c1ea
lorey2 <lorey@student.42lausanne.ch>
2026-01-15 17:29:24 +0100

    Refactor LSP configuration and add dependencies

--- lua/plugins/builtin/lsp.lua
+++ lua/plugins/builtin/lsp.lua
@@ -1,19 +1,57 @@
 return {
-	-- This is where your plugins related to LSP can be installed.
-	-- The configuration is done below. Search for lspconfig to find it below.
-	-- LSP Configuration & Plugins
-	'neovim/nvim-lspconfig',
-	dependencies = {
-		-- Automatically install LSPs to stdpath for neovim
-		{
-			'mason-org/mason.nvim',
-			config = true
-		},
-		'mason-org/mason-lspconfig.nvim',
-		{
-			"folke/lazydev.nvim",
-			ft = "lua",
-			opts = {}
-		}
-	}
+    -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        -- Automatically install LSPs to stdpath for neovim
+        { 'williamboman/mason.nvim', config = true },
+        'williamboman/mason-lspconfig.nvim',
+        
+        -- Useful status updates for LSP
+        { 'j-hui/fidget.nvim', opts = {} },
+
+        -- Lua development config
+        {
+            "folke/lazydev.nvim",
+            ft = "lua",
+            opts = {}
+        },
+        -- Capabilities for completion (assuming you use nvim-cmp)
+        'hrsh7th/cmp-nvim-lsp',
+    },
+    config = function()
+        -- 1. Setup Mason (Package Manager)
+        require('mason').setup()
+
+        -- 2. Setup Mason-LSPConfig
+        -- We explicitly REMOVE 'clangd' from this list so Mason doesn't try to download the broken binary
+        require('mason-lspconfig').setup({
+            ensure_installed = { 
+                "lua_ls", 
+                "gopls", 
+                "pyright" 
+                -- Add other servers here (html, cssls, etc.)
+                -- DO NOT add "clangd" here
+            },
+        })
+
+        -- 3. Define Capabilities (for auto-completion)
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        local lspconfig = require('lspconfig')
+
+        -- 4. Automatic Setup for Mason-Installed Servers
+        require('mason-lspconfig').setup_handlers({
+            function(server_name)
+                lspconfig[server_name].setup({
+                    capabilities = capabilities,
+                })
+            end,
+        })
+
+        -- 5. MANUAL Setup for Clangd (NixOS Fix)
+        -- This forces Neovim to use the binary from your system ($PATH)
+        lspconfig.clangd.setup({
+            capabilities = capabilities,
+            cmd = { "clangd" }, 
+        })
+    end
 }
